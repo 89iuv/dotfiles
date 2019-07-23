2fbb4c18ebae6b6227e658c62586e430bc089618
Valentin Uveges <valentin.uveges@gmail.com>
2019-07-23 11:03:24 +0000

    add zsh theme and framework, add git submodules

---
+++ git/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig
---
+++ system/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# dependencies
+sudo apt install -y git-all
+sudo apt install -y zsh
+
+
+# shell configure
+chsh -s `which zsh` `whoami`
+
