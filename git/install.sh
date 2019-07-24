f20a6e42c181f41ed55d9f6a3cd714723a4bb60c
Valentin Uveges <valentin.uveges@gmail.com>
2019-07-24 10:07:07 +0000

    centralize install scripts, use relative path in install

--- git/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-~/.dotfiles/system/install.sh
-~/.dotfiles/git/install.sh
-~/.dotfiles/zsh/install.sh
+# dependencies
+sudo apt install -y zsh fonts-powerline
+
+# vars
+user=`whoami`
+zsh=`which zsh`
+
+# shell configure
+sudo chsh -s $zsh $user
+
+# links
+ln -s git/.gitconfig ~/.gitconfig
+
+ln -s zsh/.zshrc ~/.zshrc
+ln -s zsh/powerlevel10k zsh/oh-my-zsh/custom/themes
+ln -s zsh/zsh-autosuggestions zsh/oh-my-zsh/custom/plugins
 
