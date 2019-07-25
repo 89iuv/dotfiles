86b77d5a9feb1dec20912109be5aefc21d8411e9
Valentin Uveges <valentin.uveges@gmail.com>
2019-07-25 16:57:13 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,5 +16,4 @@ ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/zsh/powerlevel10k zsh/oh-my-zsh/custom/themes
 ln -s ~/.dotfiles/zsh/zsh-autosuggestions zsh/oh-my-zsh/custom/plugins
-ln -s ~/.dotfiles/zsh/zsh-autosuggestions zsh/oh-my-zsh/custom/plugins
 ln -s ~/.dotfiles/zsh/zsh-history-substring-search zsh/oh-my-zsh/custom/plugins
