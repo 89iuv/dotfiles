07aa6c526179c1decc61d9021b6434feac15fd32
Valentin Uveges <valentin.uveges@gmail.com>
2022-08-04 08:23:47 +0300

    feat(dotfiles): set correct paths for ln

--- install.sh
+++ install.sh
@@ -25,14 +25,14 @@ path=`pwd`
 
 # links
 ln -s $path/zsh/oh-my-zsh ~/.oh-my-zsh
-ln -s $path/zsh/powerlevel10k zsh/oh-my-zsh/custom/themes
-ln -s $path/zsh/zsh-autosuggestions zsh/oh-my-zsh/custom/plugins
-ln -s $path/zsh/zsh-history-substring-search zsh/oh-my-zsh/custom/plugins
+ln -s $path/zsh/powerlevel10k ~/.oh-my-zsh/custom/themes
+ln -s $path/zsh/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins
+ln -s $path/zsh/zsh-history-substring-search ~/.oh-my-zsh/custom/plugins
 ln -s $path/zsh/.zshrc ~/.zshrc
 ln -s $path/zsh/.p10k.zsh ~/.p10k.zsh
 
 ln -s $path/tmux/.tmux.conf ~/.tmux.conf
 
-mkdir -p ~/.config/nvim/
+mkdir -p ~/.config
 ln -s $path/nvim ~/.config/nvim
 
