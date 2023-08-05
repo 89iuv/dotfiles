d143d35d0363e3047a053a6f88b2672708ad0a77
Valentin Uveges <valentin.uveges@gmail.com>
2023-08-05 07:59:52 +0300

    feat(nvchad): change separator to block, remove theme toggle button

--- nvchad/custom/chadrc.lua
+++ nvchad/custom/chadrc.lua
@@ -10,6 +10,25 @@ M.ui = {
 
   hl_override = highlights.override,
   hl_add = highlights.add,
+
+  statusline = {
+    theme = "default", -- default/vscode/vscode_colored/minimal
+
+    -- default/round/block/arrow (separators work only for "default" statusline theme;
+    -- round and block will work for the minimal theme only)
+    separator_style = "block",
+    overriden_modules = nil,
+  },
+
+  tabufline = {
+    overriden_modules = function(modules)
+      modules[4] = (function()
+        local CloseAllBufsBtn = "%@TbCloseAllBufs@%#TbLineCloseAllBufsBtn#" .. " 󰅖 " .. "%X"
+        return CloseAllBufsBtn
+      end)()
+
+    end,
+  },
 }
 
 M.plugins = "custom.plugins"
