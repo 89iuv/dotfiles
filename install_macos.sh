fb2510247b53f88e33c732a020271d247e952a2b
Valentin Uveges <valentin.uveges@gmail.com>
2024-03-21 00:20:20 +0200

    feat(nvim): extract nvchad convig in own repo

--- install_macos.sh
+++ install_macos.sh
@@ -3,14 +3,18 @@
 # dependencies
 brew install neofetch onefetch btop dust wget nvim ripgrep tmux lua fzf fd eza bat
 
-# vars
-dotfiles_path=`pwd`
+# nvchad
+git clone git@github.com:89iuv/starter.git ~/.config/nvim && nvim
+# run :MasonInstallAll
 
 # node
 # https://github.com/nvm-sh/nvm
 PROFILE=/dev/null bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash'
 nvm install node
 nvm alias default node
+
+# vars
+dotfiles_path=`pwd`
 
 # configure
 # oh-my-zsh
@@ -27,10 +31,6 @@ ln -s $dotfiles_path/zsh/.p10k.zsh ~/.p10k.zsh
 # tmux
 ln -s $dotfiles_path/tmux/.tmux.conf ~/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-# nvchad
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
-ln -s $dotfiles_path/nvchad/custom ~/.config/nvim/lua
 
 # fzf
 $(brew --prefix)/opt/fzf/install
