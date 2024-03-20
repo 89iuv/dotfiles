1bad189209d26e8108f74771206ec90e9c600201
Valentin Uveges <valentin.uveges@gmail.com>
2024-03-20 11:01:32 +0200

    chore(nvim): cleanup cmp config

--- nvchad/custom/configs/overrides.lua
+++ nvchad/custom/configs/overrides.lua
@@ -72,4 +72,15 @@ M.copilot = {
   },
 }
 
+M.cmp = {
+  sources = {
+    { name = "copilot", group_index = 2 },
+    { name = "nvim_lsp", group_index = 2 },
+    { name = "luasnip", group_index = 2 },
+    { name = "buffer", group_index = 2 },
+    { name = "nvim_lua", group_index = 2 },
+    { name = "path", group_index = 2 },
+  },
+}
+
 return M
--- nvchad/custom/plugins.lua
+++ nvchad/custom/plugins.lua
@@ -71,16 +71,7 @@ local plugins = {
         end,
       },
     },
-    opts = {
-      sources = {
-        { name = "copilot", group_index = 2 },
-        { name = "nvim_lsp", group_index = 2 },
-        { name = "luasnip", group_index = 2 },
-        { name = "buffer", group_index = 2 },
-        { name = "nvim_lua", group_index = 2 },
-        { name = "path", group_index = 2 },
-      },
-    },
+    opts = overrides.cmp,
   }
 
   -- To make a plugin not be loaded
