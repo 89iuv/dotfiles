4055d132fcf44295226a3c80c37afba3643b6d59
Valentin Uveges <valentin.uveges@gmail.com>
2024-03-20 06:59:20 +0200

    chore(nvim): cleanup and move copilot at the top of sources for cmp

--- nvchad/custom/plugins.lua
+++ nvchad/custom/plugins.lua
@@ -73,12 +73,12 @@ local plugins = {
     },
     opts = {
       sources = {
+        { name = "copilot", group_index = 2 },
         { name = "nvim_lsp", group_index = 2 },
-        { name = "copilot",  group_index = 2 },
-        { name = "luasnip",  group_index = 2 },
-        { name = "buffer",   group_index = 2 },
+        { name = "luasnip", group_index = 2 },
+        { name = "buffer", group_index = 2 },
         { name = "nvim_lua", group_index = 2 },
-        { name = "path",     group_index = 2 },
+        { name = "path", group_index = 2 },
       },
     },
   }
