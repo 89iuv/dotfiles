165066253c48f33b51607baa67bf176d03e7a483
Valentin Uveges <valentin.uveges@gmail.com>
2024-03-24 15:59:36 +0200

    feat(install): use format and lint on file

--- install_macos.sh
+++ install_macos.sh
@@ -14,23 +14,23 @@ nvm install node
 nvm alias default node
 
 # vars
-dotfiles_path=`pwd`
+dotfiles_path=$(pwd)
 
 # configure
 # oh-my-zsh
-ln -s $dotfiles_path/zsh/oh-my-zsh ~/.oh-my-zsh
-ln -s $dotfiles_path/zsh/.zshrc ~/.zshrc
+ln -s "$dotfiles_path"/zsh/oh-my-zsh ~/.oh-my-zsh
+ln -s "$dotfiles_path"/zsh/.zshrc ~/.zshrc
 
 # zsh-autosuggestions
-ln -s $dotfiles_path/zsh/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins
+ln -s "$dotfiles_path"/zsh/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins
 
 # powerlevel10k
-ln -s $dotfiles_path/zsh/powerlevel10k ~/.oh-my-zsh/custom/themes
-ln -s $dotfiles_path/zsh/.p10k.zsh ~/.p10k.zsh
+ln -s "$dotfiles_path"/zsh/powerlevel10k ~/.oh-my-zsh/custom/themes
+ln -s "$dotfiles_path"/zsh/.p10k.zsh ~/.p10k.zsh
 
 # tmux
-ln -s $dotfiles_path/tmux/.tmux.conf ~/.tmux.conf
+ln -s "$dotfiles_path"/tmux/.tmux.conf ~/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # fzf
-$(brew --prefix)/opt/fzf/install
+"$(brew --prefix)/opt/fzf/install"
