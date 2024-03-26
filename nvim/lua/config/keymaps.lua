e11b75a75fec9ed733044499aa4b63979eb69bcd
Valentin Uveges <valentin.uveges@gmail.com>
2024-03-26 09:19:26 +0200

    feat: map ; to : in nvim for faster typing

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -30,6 +30,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- faster command line
+vim.keymap.set('n', ';', ':')
+
 -- Navigation
 vim.keymap.set('n', '<M-}>', '<C-i>')
 vim.keymap.set('n', '<M-{>', '<C-o>')
