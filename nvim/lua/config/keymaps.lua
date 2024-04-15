e4855a066fe3c2415a3f530c391fdae445db400c
Valentin Uveges <valentin.uveges@gmail.com>
2024-04-15 08:52:36 +0300

    feat: remove mapping from ; to : in order to avoid confusion

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -30,9 +30,6 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- faster command line
-vim.keymap.set('n', ';', ':')
-
 -- close nvim
 vim.keymap.set('n', '<leader>Q', '<cmd>wqa!<cr>', { desc = 'Write and [Q]uit all buffers force' })
 
