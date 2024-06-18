8a73a7f9d15daa55fc6a1b8b389743466c79d3c3
Valentin Uveges <valentin.uveges@gmail.com>
2024-06-18 13:46:31 +0300

    feat(nvim): add toggle for auto format on save

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -6,24 +6,21 @@ return {
   config = function()
     vim.g.disable_autoformat = false
 
-    vim.api.nvim_create_user_command('FormatDisable', function(args)
-      if args.bang then
-        -- FormatDisable! will disable formatting just for this buffer
-        vim.b.disable_autoformat = true
-      else
-        vim.g.disable_autoformat = true
-      end
+    vim.api.nvim_create_user_command('FormatDisable', function()
+      vim.g.disable_autoformat = true
     end, {
       desc = 'Disable autoformat-on-save',
-      bang = true,
     })
 
     vim.api.nvim_create_user_command('FormatEnable', function()
-      vim.b.disable_autoformat = false
       vim.g.disable_autoformat = false
     end, {
-      desc = 'Re-enable autoformat-on-save',
+      desc = 'Enable autoformat-on-save',
     })
+
+    vim.api.nvim_create_user_command('FormatToggle', function()
+      vim.g.disable_autoformat = not vim.g.disable_autoformat
+    end, { desc = 'Toggle autoformat-on-save' })
 
     require('conform').setup {
       notify_on_error = false,
@@ -48,6 +45,7 @@ return {
     require('which-key').register {
       ['<leader>f'] = { name = '[F]ormat', _ = 'which_key_ignore' },
     }
+    vim.keymap.set('n', '<leader>tf', '<ESC><cmd>FormatToggle<CR>', { desc = '[T]oggle Auto [F]format on Save' })
     vim.keymap.set('n', '<leader>ff', '<ESC><cmd>lua require("conform").format { lsp_fallback = true } <CR>', { desc = '[F]ormat [F]ile' })
     vim.keymap.set('v', '<leader>fr', '<ESC><cmd>lua require("conform").format { lsp_fallback = true } <CR>', { desc = '[F]ormat [R]ange' })
   end,
