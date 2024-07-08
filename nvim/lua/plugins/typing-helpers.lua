236fc5afe586d39b8146ac32bc6a1061b5f20736
Valentin Uveges <valentin.uveges@gmail.com>
2024-07-08 17:58:52 +0300

    feat(nvim): change padding for mini start

--- nvim/lua/plugins/typing-helpers.lua
+++ nvim/lua/plugins/typing-helpers.lua
@@ -99,7 +99,7 @@ return {
         content_hooks = {
           starter.gen_hook.adding_bullet(),
           starter.gen_hook.indexing('all', { 'Builtin actions' }),
-          starter.gen_hook.padding(3, 2),
+          starter.gen_hook.padding(1, 1),
         },
       }
     end,
