59f072cdd0c8067d8be39f16bd6233d0b3559c7f
Valentin Uveges <valentin.uveges@gmail.com>
2024-10-19 16:38:42 +0300

    feat(nvim): customize lazyvim

---
+++ nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,15 @@
+return {
+  "akinsho/bufferline.nvim",
+  opts = {
+    options = {
+      offsets = {
+        {
+          filetype = "neo-tree",
+          -- text = "Neo-tree",
+          -- highlight = "Directory",
+          text_align = "left",
+        },
+      },
+    },
+  },
+}
---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = {
+    options = {
+      component_separators = { left = "/", right = "|" },
+      section_separators = { left = "", right = "▐" },
+    },
+  },
+}
