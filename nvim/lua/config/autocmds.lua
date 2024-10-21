eb1ec68e4d193b1b80575d980b24b1d18d4be8d2
Valentin Uveges <valentin.uveges@gmail.com>
2024-10-21 12:39:43 +0300

    feat(nvim): add support for copilot chat

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Do not continue with comments on the next line
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    vim.opt_local.formatoptions:remove({ "r", "o" })
+  end,
+})
--- nvim/lua/plugins/lazyvim.lua
+++ nvim/lua/plugins/lazyvim.lua
@@ -1,5 +1,8 @@
 return {
   {
+    dependencies = {
+      { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    },
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin-macchiato",
