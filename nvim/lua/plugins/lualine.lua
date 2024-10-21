6ddbdb7e1930af3c7fece048bfeb4121ab437cf3
Valentin Uveges <valentin.uveges@gmail.com>
2024-10-21 13:48:41 +0300

    feat(tmux): update catppuccin tmux to 2.0

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,8 +2,8 @@ return {
   "nvim-lualine/lualine.nvim",
   opts = {
     options = {
-      component_separators = { left = "/", right = "|" },
-      section_separators = { left = "", right = "▐" },
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
     },
   },
 }
