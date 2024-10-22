cd9ca81a558836650134861dd8d5196c82d944c5
Valentin Uveges <valentin.uveges@gmail.com>
2024-10-22 10:14:40 +0300

    feat(nvim): disable lazygit theme from nvim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- LazyVim automatically configures lazygit:
+--  * theme, based on the active colorscheme.
+--  * editPreset to nvim-remote
+--  * enables nerd font icons
+-- Set to false to disable.
+-- Set the options you want to override in `~/.config/lazygit/custom.yml`
+-- WARN: on Windows you might want to set `editPreset: "nvim"` due to
+-- this issue https://github.com/jesseduffield/lazygit/issues/3467
+vim.g.lazygit_config = false
