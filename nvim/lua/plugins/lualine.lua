b0e97b393b9534b7e0e41cd8dd9d7891d52bc9d3
Valentin Uveges <valentin.uveges@gmail.com>
2024-10-28 18:02:21 +0200

    feat(nvim): cleanup lualine, config cmp to: add super tab mapping, disable transparency, disable insert on previous and next entry

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -5,129 +5,17 @@ return {
   },
   init = function()
     vim.g.trouble_lualine = false
+    vim.opt.pumblend = 0 -- disable cmp menu transparency
   end,
-  opts = function()
+  opts = function(_, opts)
     local macchiato = require("catppuccin.palettes").get_palette("macchiato")
     local lualine_catppuccin = require("catppuccin.utils.lualine")()
 
     lualine_catppuccin.normal.c.bg = macchiato.crust
     lualine_catppuccin.inactive.c.bg = macchiato.crust
 
-    -- PERF: we don't need this lualine require madness 🤷
-    local lualine_require = require("lualine_require")
-    lualine_require.require = require
-
-    local icons = LazyVim.config.icons
-
-    vim.o.laststatus = vim.g.lualine_laststatus
-
-    local opts = {
-      options = {
-        theme = lualine_catppuccin,
-        globalstatus = vim.o.laststatus == 3,
-        component_separators = { left = "│", right = "│" },
-        section_separators = { left = "▌", right = "▐" },
-        disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter" } },
-      },
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch" },
-
-        lualine_c = {
-          LazyVim.lualine.root_dir({ icon = "󱉭" }),
-          {
-            "diagnostics",
-            symbols = {
-              error = icons.diagnostics.Error,
-              warn = icons.diagnostics.Warn,
-              info = icons.diagnostics.Info,
-              hint = icons.diagnostics.Hint,
-            },
-          },
-          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-          {
-            LazyVim.lualine.pretty_path(),
-            separator = "",
-            padding = { left = 0, right = 0 },
-          },
-        },
-        lualine_x = {
-          -- stylua: ignore
-          {
-            function() return require("noice").api.status.command.get() end,
-            cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-            color = function() return LazyVim.ui.fg("Statement") end,
-          },
-          -- stylua: ignore
-          {
-            function() return require("noice").api.status.mode.get() end,
-            cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-            color = function() return LazyVim.ui.fg("Constant") end,
-          },
-          -- stylua: ignore
-          {
-            function() return "  " .. require("dap").status() end,
-            cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
-            color = function() return LazyVim.ui.fg("Debug") end,
-          },
-          -- stylua: ignore
-          {
-            require("lazy.status").updates,
-            cond = require("lazy.status").has_updates,
-            color = function() return LazyVim.ui.fg("Special") end,
-          },
-          {
-            "diff",
-            symbols = {
-              added = icons.git.added,
-              modified = icons.git.modified,
-              removed = icons.git.removed,
-            },
-            source = function()
-              local gitsigns = vim.b.gitsigns_status_dict
-              if gitsigns then
-                return {
-                  added = gitsigns.added,
-                  modified = gitsigns.changed,
-                  removed = gitsigns.removed,
-                }
-              end
-            end,
-          },
-        },
-        lualine_y = {
-          { "progress", separator = " ", padding = { left = 1, right = 0 } },
-          { "location", padding = { left = 0, right = 1 } },
-        },
-        lualine_z = {
-          function()
-            return " " .. os.date("%R")
-          end,
-        },
-      },
-      extensions = { "neo-tree", "lazy" },
-    }
-
-    -- do not add trouble symbols if aerial is enabled
-    -- And allow it to be overriden for some buffer types (see autocmds)
-    if vim.g.trouble_lualine and LazyVim.has("trouble.nvim") then
-      local trouble = require("trouble")
-      local symbols = trouble.statusline({
-        mode = "symbols",
-        groups = {},
-        title = false,
-        filter = { range = true },
-        format = "{kind_icon}{symbol.name:Normal}",
-        hl_group = "lualine_c_normal",
-      })
-      table.insert(opts.sections.lualine_c, {
-        symbols and symbols.get,
-        cond = function()
-          return vim.b.trouble_lualine ~= false and symbols.has()
-        end,
-      })
-    end
-
-    return opts
+    opts.options.theme = lualine_catppuccin
+    opts.options.component_separators = { left = "│", right = "│" }
+    opts.options.section_separators = { left = "▌", right = "▐" }
   end,
 }
---
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,51 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      opts.mapping = vim.tbl_extend(
+        "force",
+        opts.mapping,
+        cmp.mapping.preset.insert({
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+              cmp.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace })
+            elseif vim.snippet.active({ direction = 1 }) then
+              vim.schedule(function()
+                vim.snippet.jump(1)
+              end)
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif vim.snippet.active({ direction = -1 }) then
+              vim.schedule(function()
+                vim.snippet.jump(-1)
+              end)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        })
+      )
+
+      opts.experimental.ghost_text = false
+    end,
+  },
+}
