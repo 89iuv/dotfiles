b56283bef0b241e929602bfd7bebcf8302c4de0b
Valentin Uveges <valentin.uveges@gmail.com>
2024-10-31 08:16:22 +0200

    feat(nvim): make neo-tree equalize window size on close and open

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,7 +1,29 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    event_handlers = {
+      {
+        event = "neo_tree_window_after_open",
+        handler = function(args)
+          if args.position == "left" or args.position == "right" then
+            vim.cmd("wincmd =")
+          end
+        end,
+      },
+      {
+        event = "neo_tree_window_after_close",
+        handler = function(args)
+          if args.position == "left" or args.position == "right" then
+            vim.cmd("wincmd =")
+          end
+        end,
+      },
+    },
     filesystem = {
+      follow_current_file = {
+        enabled = true,
+        leave_dirs_open = true,
+      },
       filtered_items = {
         visible = true,
         hide_dotfiles = false,
