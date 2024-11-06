366dfa4fd9003ff937620abe7ba115abc58d8d94
Valentin Uveges <valentin.uveges@gmail.com>
2024-11-06 20:12:04 +0200

    feat(nvim): add support for typescript, update submodules

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -25,6 +25,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.formatting.black" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.coding.copilot-chat" },
