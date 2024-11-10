3627803edb2250d70a2e52d545aa78f230a8d1c1
Valentin Uveges <valentin.uveges@gmail.com>
2024-11-10 14:45:35 +0200

    feat(nvim): make noice windows close by using esc key

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -8,7 +8,17 @@ return {
       lsp_doc_border = true,
     },
     views = {
-      split = { scrollbar = false },
+      split = {
+        scrollbar = false,
+        close = {
+          keys = { "<esc>", "q" },
+        },
+      },
+      popup = {
+        close = {
+          keys = { "<esc>", "q" },
+        },
+      },
     },
   },
 }
