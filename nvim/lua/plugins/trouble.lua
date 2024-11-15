6a364abefe88b3003266d6cb601b2d018492f5c9
Valentin Uveges <valentin.uveges@gmail.com>
2024-11-15 17:07:43 +0200

    feat(nvm): make trouble close on exit

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -4,21 +4,28 @@ return {
   opts = {
     modes = {
       lsp = {
+        auto_preview = false,
+        auto_refresh = false,
+        focus = true,
+        open_no_results = true,
         win = {
           position = "bottom",
           size = 20,
         },
       },
       symbols = {
+        auto_preview = true,
+        auto_refresh = true,
+        focus = true,
+        open_no_results = true,
         win = {
           position = "right",
           size = 45,
         },
       },
     },
-    auto_preview = false,
-    auto_refresh = false,
-    focus = true,
-    open_no_results = true,
+    keys = {
+      ["<esc>"] = "close",
+    },
   },
 }
