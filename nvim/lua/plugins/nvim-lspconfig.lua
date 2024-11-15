f452ecd482161bf7cc7e8b06390e071053dd9a81
Valentin Uveges <valentin.uveges@gmail.com>
2024-11-15 15:51:30 +0200

    feat(nvim): add support for robotframework

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,9 +1,21 @@
+local python_path = vim.system({ "which", "python" }, { text = true, timeout = 5000 }):wait().stdout
+local settings = {}
+if python_path then
+  python_path = string.gsub(python_path, "\n", "")
+  settings = {
+    ["robot.python.executable"] = python_path,
+  }
+end
+
 return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
       bashls = {
         filetypes = { "sh", "zsh" },
+      },
+      robotframework_ls = {
+        settings = settings,
       },
     },
   },
---
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,8 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "robot",
+    },
+  },
+}
