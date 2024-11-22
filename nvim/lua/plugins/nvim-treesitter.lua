937bf840735683933ea0b605610bfa99dff4068a
Valentin Uveges <valentin.uveges@gmail.com>
2024-11-22 20:27:28 +0200

    feat(nvim): change keys for increment and decrement selection

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -4,5 +4,14 @@ return {
     ensure_installed = {
       "robot",
     },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<space>o",
+        node_incremental = "<space>o",
+        scope_incremental = false,
+        node_decremental = "<space>i",
+      },
+    },
   },
 }
--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -2,5 +2,9 @@ return {
   "folke/which-key.nvim",
   opts = {
     preset = "modern",
+    spec = {
+      { "<space>i", desc = "Decrement Selection", mode = "x" },
+      { "<space>o", desc = "Increment Selection", mode = { "x", "n" } },
+    },
   },
 }
