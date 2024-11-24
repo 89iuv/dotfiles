cd33e93bf824b0515f748fb307d77f9c5cad92e2
Valentin Uveges <valentin.uveges@gmail.com>
2024-11-24 10:52:04 +0200

    feat(nvim): add markdown-render to copilot-chat

---
+++ nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,7 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  ft = { "markdown", "norg", "rmd", "org", "copilot-chat" },
+  opts = {
+    file_types = { "markdown", "copilot-chat" },
+  },
+}
