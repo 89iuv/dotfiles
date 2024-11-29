682627a0777c3a20dff2746cfbce01cbb840cc9b
Valentin Uveges <valentin.uveges@gmail.com>
2024-11-29 07:58:19 +0200

    feat(nvim): add telescope key for finding spell suggestions

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,5 +1,12 @@
 return {
   "nvim-telescope/telescope.nvim",
+  keys = {
+    {
+      "<leader>fs",
+      "<cmd>Telescope spell_suggest<cr>",
+      desc = "Find Spell Suggestions",
+    },
+  },
   opts = {
     defaults = {
       layout_config = {
