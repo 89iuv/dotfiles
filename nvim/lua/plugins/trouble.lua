1dd5f04ef12ecc2e1f91a7190b2bfc5a0fb52b06
Valentin Uveges <valentin.uveges@gmail.com>
2024-12-02 10:30:19 +0200

    feat(nvim): trouble clean up

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -8,20 +8,12 @@ return {
     open_no_results = true,
     modes = {
       lsp = {
-        auto_preview = false,
-        auto_refresh = false,
-        focus = true,
-        open_no_results = true,
         win = {
           position = "bottom",
           size = 20,
         },
       },
       symbols = {
-        auto_preview = true,
-        auto_refresh = true,
-        focus = true,
-        open_no_results = true,
         win = {
           position = "right",
           size = 45,
