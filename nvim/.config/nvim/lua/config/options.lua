5df9351bb946f6741fdcff21d68ef18687a89d03
Valentin Uveges <valentin.uveges@gmail.com>
2025-03-20 11:02:16 +0200

    revert(nvim): revert settings for lazyvim root spec

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,7 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.ua
 -- Add any additiona options here
 
-vim.g.root_spec = { "cwd" }
+-- make lazy use the cwd as root
+-- cwd is the directory where you start nvim
+-- root is the project root (determined by lsp, .git, etc.) for the opened file
+-- use ":LazyRoot" to identify the project root
+-- note: the project root si always bellow or equal to the cwd
+-- vim.g.root_spec = { "cwd" }
 
 vim.g.snacks_animate = false
 vim.g.trouble_lualine = false
@@ -13,6 +18,7 @@ vim.opt.relativenumber = false
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
+-- format for folded lines
 vim.opt.foldtext = 'v:lua.CustomFoldText()'
 function _G.CustomFoldText()
   local line = vim.fn.getline(vim.v.foldstart)
