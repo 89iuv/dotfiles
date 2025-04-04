84eb76b42a98a69f81d6bfabc8df8542064082e8
Valentin Uveges <valentin.uveges@gmail.com>
2025-04-04 15:57:16 +0300

    feat(term): add config cmd to alt passthrough

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,25 +1,47 @@
--- Pull in the wezterm API
-local wezterm = require 'wezterm'
-
--- This will hold the configuration.
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
+-- color scheme
 config.color_scheme = "Catppuccin Mocha" -- or Macchiato, Frappe, Latte
 
-config.font = wezterm.font 'JetBrainsMono Nerd Font'
-config.font_size = 15
+-- font
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 13
 
--- config.enable_tab_bar = false
--- config.window_decorations = "RESIZE"
-
+-- window decorations
 config.enable_tab_bar = true
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 
-
-config.initial_cols = 128
-config.initial_rows = 38
+-- window size
+config.initial_cols = 120
+config.initial_rows = 32
 config.use_resize_increments = false
+
+-- paste options
+-- rewrite line endings to LF when pasting
+config.canonicalize_pasted_newlines = "LineFeed"
+
+-- keymaps
+local normal_keys = { ",", "." }
+local shift_keys = { "<", ">" }
+
+local insert_key_pass = function(conf, keys, modifier)
+	modifier = modifier and "|" .. modifier or ""
+	for _, key in ipairs(keys) do
+		table.insert(conf, {
+			key = key,
+			mods = "SUPER",
+			action = wezterm.action.SendKey({
+				key = key,
+				mods = "ALT",
+			}),
+		})
+	end
+end
+
+config.keys = {}
+insert_key_pass(config.keys, normal_keys)
+insert_key_pass(config.keys, shift_keys, "SHIFT")
 
 -- and finally, return the configuration to wezterm
 return config
