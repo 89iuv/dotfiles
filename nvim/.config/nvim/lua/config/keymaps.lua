7c03d4d8f02a0450566b92cbf9670dd2666bfd55
Valentin Uveges <valentin.uveges@gmail.com>
2025-04-07 17:06:41 +0300

    feat(nvim): add J,K for scrolling in visual mode

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -5,6 +5,10 @@
 -- workaround for <leader>wd not working on fast action
 vim.keymap.set("n", "<leader>w", "<nop>", { desc = "Windows", remap = false })
 
--- use Shift + j,k instead of Ctrl+u,d for scrolling
+-- use Shift + j,k instead of Ctrl+u,d for scrolling in normal mode
 vim.keymap.set("n", "J", "<C-d>", { remap = false})
 vim.keymap.set("n", "K", "<C-u>", { remap = false})
+
+-- use Shift + j,k instead of Ctrl+u,d for scrolling in visual mode
+vim.keymap.set("v", "J", "<C-d>", { remap = false})
+vim.keymap.set("v", "K", "<C-u>", { remap = false})
