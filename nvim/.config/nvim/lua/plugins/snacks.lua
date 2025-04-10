1276bbf0d518d0be9bf5a423770a1b7ed0a9cd44
Valentin Uveges <valentin.uveges@gmail.com>
2025-04-10 16:28:36 +0300

    feat(nvim): configure snacks dashboard

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -3,6 +3,16 @@ return {
   opts = {
     dashboard = {
       preset = {
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})", },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
         header = [[
 ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
 ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
@@ -57,7 +67,7 @@ return {
         height = 0.30,
         wo = {
           -- winbar = "",
-          winhighlight = "Normal:Normal,WinBar:SnacksWinBar",
+          -- winhighlight = "Normal:Normal,WinBar:SnacksWinBar",
         },
         keys = {
           term_normal = {
