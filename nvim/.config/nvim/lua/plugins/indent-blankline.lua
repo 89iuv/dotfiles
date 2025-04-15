738a66fbc441e92dda6b37cc8022780b4a6720a7
Valentin Uveges <valentin.uveges@gmail.com>
2025-04-15 20:21:23 +0300

    feat(nvim): make ibl and mini indentscope work the same

--- nvim/.config/nvim/lua/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -1,12 +1,67 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   enabled = true,
-  opts = {
-    exclude = {
-      filetypes = {
-        "markdown",
-        "snacks_picker_preview",
+  config = function()
+    Snacks.toggle({
+      name = "Indention Guides",
+      get = function()
+        return require("ibl.config").get_config(0).enabled
+      end,
+      set = function(state)
+        require("ibl").setup_buffer(0, { enabled = state })
+      end,
+    }):map("<leader>ug")
+
+    require("ibl").setup({
+      indent = {
+        char = "│",
+        tab_char = "│",
       },
-    },
-  },
+      scope = { enabled = false },
+    })
+
+    require("ibl").overwrite({
+      exclude = {
+        filetypes = {
+          -- default
+          "lspinfo",
+          "packer",
+          "checkhealth",
+          "help",
+          "man",
+          "gitcommit",
+          "TelescopePrompt",
+          "TelescopeResults",
+          "",
+
+          -- lazy
+          "Trouble",
+          "alpha",
+          "dashboard",
+          "fzf",
+          "help",
+          "lazy",
+          "mason",
+          "neo-tree",
+          "notify",
+          "snacks_dashboard",
+          "snacks_notif",
+          "snacks_terminal",
+          "snacks_win",
+          "toggleterm",
+          "trouble",
+
+          -- custom
+          "markdown",
+          "snacks_picker_preview",
+          "snacks_picker_list",
+        },
+        buftypes = {
+          "terminal",
+          "quickfix",
+          "prompt",
+        },
+      },
+    })
+  end,
 }
--- nvim/.config/nvim/lua/plugins/mini-indentscope.lua
+++ nvim/.config/nvim/lua/plugins/mini-indentscope.lua
@@ -5,15 +5,58 @@ return {
     -- disable mini indentscope for filetypes
     vim.api.nvim_create_autocmd("FileType", {
       pattern = {
+        -- default
+        "lspinfo",
+        "packer",
         "checkhealth",
+        "help",
+        "man",
+        "gitcommit",
+        "TelescopePrompt",
+        "TelescopeResults",
+        "",
+
+        -- lazy
+        "Trouble",
+        "alpha",
+        "dashboard",
+        "fzf",
+        "help",
+        "lazy",
+        "mason",
+        "neo-tree",
+        "notify",
+        "snacks_dashboard",
+        "snacks_notif",
+        "snacks_terminal",
+        "snacks_win",
+        "toggleterm",
+        "trouble",
+
+        -- custom
         "markdown",
-        "grug-far-help",
         "snacks_picker_preview",
-        "noice",
+        "snacks_picker_list",
       },
       callback = function()
         vim.b.miniindentscope_disable = true
       end,
+    })
+
+    -- disable mini indentscope for buftypes
+    vim.api.nvim_create_autocmd("BufEnter", {
+      callback = function ()
+        local patterns = {
+          "terminal",
+          "quickfix",
+          "prompt",
+        }
+        for _, pattern in ipairs(patterns) do
+          if pattern == vim.bo.buftype then
+            vim.b.miniindentscope_disable = true
+          end
+        end
+      end
     })
 
     -- set custom border for filetype
