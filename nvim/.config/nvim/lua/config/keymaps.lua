91da86740000561f8aa30d4d73b39a82a78b9f78
Valentin Uveges <valentin.uveges@gmail.com>
2025-04-17 08:15:33 +0300

    feat(nvim): add keymap for search selection under cursor

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -5,5 +5,5 @@
 -- workaround for <leader>wd not working on fast action
 vim.keymap.set("n", "<leader>w", "<nop>", { desc = "Windows", remap = false })
 
- -- Highlight the word under the coursor
-vim.api.nvim_set_keymap('n', '*', '*N', {noremap = true})
+vim.keymap.set('n', '*', '*N', {noremap = true, desc = "Search word unde cursor"})
+vim.keymap.set('v', '*', 'y/\\V<C-r>"<CR>N' , {noremap = true , desc = "Search selection under cursor" })
