3bd520cba1e6bac32a534fdee4ce74f7416da040
Valentin Uveges <valentin.uveges@gmail.com>
2025-06-25 22:08:18 +0300

    feat(home): switch to unstable

--- home-manager/.config/home-manager/flake.nix
+++ home-manager/.config/home-manager/flake.nix
@@ -3,9 +3,9 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
