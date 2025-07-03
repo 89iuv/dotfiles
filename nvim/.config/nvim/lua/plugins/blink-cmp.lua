30228f766018d6d4f0d54f9bc39de7f7200cbaba
Valentin Uveges <valentin.uveges@gmail.com>
2025-07-03 21:58:20 +0300

    feat(nvim): make blink dictionary work in code comments

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -1,3 +1,41 @@
+-- Use this function to check if the cursor is inside a comment block
+local function inside_comment_block()
+  if vim.api.nvim_get_mode().mode ~= "i" then
+    return false
+  end
+  local node_under_cursor = vim.treesitter.get_node()
+  local parser = vim.treesitter.get_parser(nil, nil, { error = false })
+  local query = vim.treesitter.query.get(vim.bo.filetype, "highlights")
+  if not parser or not node_under_cursor or not query then
+    return false
+  end
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  row = row - 1
+  for id, node, _ in query:iter_captures(node_under_cursor, 0, row, row + 1) do
+    if query.captures[id]:find("comment") then
+      local start_row, start_col, end_row, end_col = node:range()
+      if start_row <= row and row <= end_row then
+        if start_row == row and end_row == row then
+          if start_col <= col and col <= end_col then
+            return true
+          end
+        elseif start_row == row then
+          if start_col <= col then
+            return true
+          end
+        elseif end_row == row then
+          if col <= end_col then
+            return true
+          end
+        else
+          return true
+        end
+      end
+    end
+  end
+  return false
+end
+
 return {
   "saghen/blink.cmp",
   dependencies = {
@@ -50,7 +88,12 @@ return {
       default = function()
         -- put those which will be shown always
         local result = { "lsp", "path", "snippets", "buffer" }
-        if vim.tbl_contains({ "markdown", "text" }, vim.bo.filetype) then
+        if
+          -- turn on dictionary in markdown or text file
+          vim.tbl_contains({ "markdown", "text" }, vim.bo.filetype)
+          -- or turn on dictionary if cursor is in the comment block
+          or inside_comment_block()
+        then
           table.insert(result, "dictionary")
         end
         return result
