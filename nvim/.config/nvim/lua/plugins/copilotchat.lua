902f95446a764dbf5f989361da3230d1ef4ef871
Valentin Uveges <valentin.uveges@gmail.com>
2025-07-10 15:15:20 +0300

    feat(nvim): add back header highlight and enable copilot chat autocomplete

--- nvim/.config/nvim/lua/plugins/copilotchat.lua
+++ nvim/.config/nvim/lua/plugins/copilotchat.lua
@@ -2,12 +2,12 @@ return {
   "CopilotC-Nvim/CopilotChat.nvim",
   build = "make tiktoken",
   opts = {
-    highlight_headers = false,
+    highlight_headers = true,
     separator = string.rep("─", 3),
     error_header = "> [!ERROR] Error",
 
     insert_at_end = true,
-    chat_autocomplete = false,
+    chat_autocomplete = true,
 
     model = "gpt-4.1",
     context = "buffer",
