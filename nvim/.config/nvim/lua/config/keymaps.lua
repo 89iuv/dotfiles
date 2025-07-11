4eacec4541dda062afa617e8c76b8e9bf0355451
Valentin Uveges <valentin.uveges@gmail.com>
2025-07-11 23:01:14 +0300

    chore(nvim): remove work around for window keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,9 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- work around for deleting window not working on fast action
-vim.keymap.set("n", "<leader>w", "<nop>", { desc = "Windows", remap = false })
-
 -- move in insert using ctrl+hjkl
 vim.keymap.set("i", "<C-h>", "<Left>", { noremap = true, silent = true })
 vim.keymap.set("i", "<C-j>", "<Down>", { noremap = true, silent = true })
