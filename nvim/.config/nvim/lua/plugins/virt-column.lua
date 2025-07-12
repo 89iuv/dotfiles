fdcc34614d1c0f58700b2a42d8ad2e0a2e2589e5
Valentin Uveges <valentin.uveges@gmail.com>
2025-07-12 14:45:19 +0300

    fix(nvim): hack around virt-columns in order to render it in markdown only in insert mode

--- nvim/.config/nvim/lua/plugins/virt-column.lua
+++ nvim/.config/nvim/lua/plugins/virt-column.lua
@@ -25,11 +25,70 @@ return {
     end
   end,
   opts = function()
+    -- WARN: ugly hack in order to let the buffer overwrite the config
+    local M = require("virt-column.config")
+    local utils = require("virt-column.utils")
+
+    ---@param behavior "merge"|"overwrite"
+    ---@param base virtcolumn.config.full
+    ---@param input virtcolumn.config?
+    ---@return virtcolumn.config.full
+    local merge_configs = function(behavior, base, input)
+      local result = vim.tbl_deep_extend("keep", input or {}, base) --[[@as virtcolumn.config.full]]
+
+      if behavior == "merge" and input then
+        result.exclude.filetypes = utils.tbl_join(base.exclude.filetypes, vim.tbl_get(input, "exclude", "filetypes"))
+        result.exclude.buftypes = utils.tbl_join(base.exclude.buftypes, vim.tbl_get(input, "exclude", "buftypes"))
+      end
+
+      return result
+    end
+
+    --- Returns the configuration for a buffer
+    ---
+    ---@param bufnr number
+    ---@return virtcolumn.config.full
+    ---@diagnostic disable-next-line: duplicate-set-field
+    M.get_config = function(bufnr)
+      bufnr = utils.get_bufnr(bufnr)
+      return merge_configs("overwrite", M.config or M.default_config, M.buffer_config[bufnr])
+    end
+
+    vim.api.nvim_create_autocmd("ModeChanged", {
+      pattern = "*:n",
+      callback = function(event)
+        if vim.bo[event.buf].filetype == "markdown" then
+          require("virt-column").setup_buffer(event.buf, {
+            enabled = false,
+          })
+        end
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("ModeChanged", {
+      pattern = "n:*",
+      callback = function(event)
+        if vim.bo[event.buf].filetype == "markdown" then
+          require("virt-column").setup_buffer(event.buf, {
+            enabled = vim.g.virt_column,
+            exclude = {
+              filetypes = {},
+            },
+          })
+        end
+      end,
+    })
+
     return {
       enabled = vim.g.virt_column,
       char = { "┃", "│" },
       virtcolumn = "80,120",
       highlight = "VirtColumn",
+      exclude = {
+        filetypes = {
+          "markdown",
+        },
+      },
     }
   end,
 }
