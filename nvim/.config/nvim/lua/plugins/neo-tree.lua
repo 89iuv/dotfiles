c552a0cb9728917af327732e55ed75114a7193af
Valentin Uveges <valentin.uveges@gmail.com>
2025-08-01 08:50:44 +0300

    feat(nvim): integrate new note function in neo-tree

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -13,18 +13,31 @@ end
 local create_new_note = function(state)
   local title = "Note - " .. os.date()
 
-  local path = state.tree:get_node().path
-  local filename = title .. ".md"
-  local full_path = path .. "/" .. filename
-  local header = "# " .. title
-
-  local file = io.open(full_path, "w")
-  if file then
-    file:write(header)
-    file:close()
+  local path
+  if state.tree:get_node().type == "directory" then
+    path = state.tree:get_node():get_id()
+  else
+    path = state.tree:get_node():get_parent_id()
   end
 
-  vim.cmd("edit" .. " " .. full_path)
+  local filename = title .. ".md"
+  local header = "# " .. title
+
+  local neo_tree_inputs = require("neo-tree.ui.inputs")
+  neo_tree_inputs.input("Enter name for new note (defaults to the current timestamp):", filename, function(input)
+    if input and input ~= "" then
+      local full_path = path .. "/" .. input
+      local file = io.open(full_path, "w")
+      if file then
+        file:write(header)
+        file:close()
+      end
+      require("neo-tree.command").execute({
+        reveal_file = full_path, -- path to file or folder to reveal
+        reveal_force_cwd = true, -- change cwd without asking if needed
+      })
+    end
+  end)
 end
 
 return {
@@ -134,7 +147,7 @@ return {
           function(state)
             create_new_note(state)
           end,
-          desc = "create_new_note"
+          desc = "create_new_note",
         },
       },
     },
@@ -156,7 +169,7 @@ return {
           local fc = require("neo-tree.sources.filesystem.components")
           local result = fc.name(config, node, state)
           if node:get_depth() == 1 and node.type ~= "message" then
-            local project_path = result.text
+            local project_path = "[" .. string.sub(result.text, 1, -2) .. "]"
             local project_name = vim.fn.fnamemodify(node.path, ":t")
             result.text = project_name .. " " .. project_path
             result = {
