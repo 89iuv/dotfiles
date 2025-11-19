0a7dfda3d6e548626964f63a887676cc1938139c
Valentin Uveges <valentin.uveges@gmail.com>
2025-11-19 21:58:07 +0200

    fix(nvim): pin todo-comments due to highlight bug in markdown-render

---
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -0,0 +1,5 @@
+return {
+  "folke/todo-comments.nvim",
+  -- TODO: pin to this version because of: https://github.com/folke/todo-comments.nvim/issues/379
+  commit = "411503d3bedeff88484de572f2509c248e499b38",
+}
