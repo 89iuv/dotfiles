be155ec364bb466f98aa4a1f2f644dfe126e7a62
Valentin Uveges <valentin.uveges@gmail.com>
2025-12-16 08:46:06 +0200

    debug(github): add more info

--- catppuccin-bat/install.sh
+++ catppuccin-bat/install.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 mkdir -p "$(bat --config-dir)/themes"
+
+echo "debug: pwd"
+pwd
+
+echo "debug: ls"
 ls
+
+echo "debug: ls ./bat/"
 ls ./bat/
+
+echo "debug: ls ./bat/themes"
 ls ./bat/themes/
+
 cp ./bat/themes/Catppuccin* "$(bat --config-dir)/themes"
 bat cache --build
