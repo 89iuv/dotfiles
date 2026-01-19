e408e0bdc8365fdaca96a6f42963ee172c3dfc83
Valentin Uveges <valentin.uveges@gmail.com>
2026-01-19 22:21:01 +0200

    feat(nvim): add keymaps for diffthis and diffoff on all windows

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -8,3 +8,8 @@ vim.keymap.set("n", "<M-Down>", ":resize -2<CR>", { silent = true, desc = "Decre
 vim.keymap.set("n", "<M-Left>", ":vertical resize -2<CR>", { silent = true, desc = "Decrease window width" })
 vim.keymap.set("n", "<M-Right>", ":vertical resize +2<CR>", { silent = true, desc = "Increase window width" })
 
+-- Diff windows
+local wk = require("which-key")
+wk.add({ { "<leader>i", icon = { icon = "󰢪 ", color = "yellow" }, group = "diff" } })
+vim.keywap.set("n", "<leader>it", ":windo diffthis<CR>", { silent = true, desc = "Diff This on all Windows" })
+vim.keymap.set("n", "<leader>io", ":windo diffoff<CR>", { silent = true, desc = "Diff Off on all Windows" })
