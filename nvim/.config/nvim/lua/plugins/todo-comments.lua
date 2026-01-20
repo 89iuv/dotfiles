fa4999481f8f058d8bf3566b5f9f4845f0d83ffe
Valentin Uveges <valentin.uveges@gmail.com>
2026-01-20 06:58:50 +0200

    fix(nvim): remove todo-comments version pin as issue has been fixed

--- nvim/.config/nvim/lua/plugins/todo-comments.lua
+++
@@ -1,5 +0,0 @@
-return {
-  "folke/todo-comments.nvim",
-  -- TODO: pin to this version because of: https://github.com/folke/todo-comments.nvim/issues/379
-  commit = "411503d3bedeff88484de572f2509c248e499b38",
-}
