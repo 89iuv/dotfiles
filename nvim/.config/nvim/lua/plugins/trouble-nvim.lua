1aa5a4053eddaf0dea4e6e459c28328582ae0214
Valentin Uveges <valentin.uveges@gmail.com>
2026-01-21 14:28:51 +0200

    feat(nvim): add keymaps and style for trouble hierachy

--- nvim/.config/nvim/lua/plugins/trouble-nvim.lua
+++ nvim/.config/nvim/lua/plugins/trouble-nvim.lua
@@ -1,26 +1,66 @@
 return {
   "folke/trouble.nvim",
+  keys = {
+    {
+      "<leader>ch",
+      desc = "Lsp Hierarchy (Trouble)",
+    },
+    {
+      "<leader>chi",
+      "<cmd>Trouble lsp_incoming_calls<CR>",
+      desc = "Lsp Incoming Calls",
+    },
+    {
+      "<leader>cho",
+      "<cmd>Trouble lsp_outgoing_calls<CR>",
+      desc = "Lsp Outgoing Calls",
+    },
+    {
+      "<leader>chr",
+      "<cmd>Trouble lsp_references<CR>",
+      desc = "Lsp References",
+    },
+  },
   opts = {
-    focus = true,
-    auto_preview = false,
     auto_refresh = false,
     modes = {
       lsp = {
+        focus = true,
         preview = {
           type = "split",
           relative = "win",
           position = "right",
-          size = 0.5,
+          size = 0.4,
+          wo = {
+            number = true,
+          },
         },
         win = {
           position = "bottom",
-          size = 0.30,
+          size = 0.35,
+        },
+      },
+      lsp_base = {
+        focus = true,
+        preview = {
+          type = "split",
+          relative = "win",
+          position = "right",
+          size = 0.4,
+          wo = {
+            number = true,
+          },
+        },
+        win = {
+          position = "bottom",
+          size = 0.35,
         },
       },
       symbols = {
         focus = true,
+        auto_preview = false,
         win = {
-          size = 40,
+          size = 45,
         },
         format = "{kind_icon} {symbol.name}",
       },
