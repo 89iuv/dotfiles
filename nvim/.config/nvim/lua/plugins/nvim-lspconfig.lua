c8b43e86680e38e1666c2b389d1ecdae786db054
Valentin Uveges <valentin.uveges@gmail.com>
2026-01-27 07:54:18 +0200

    fix(nvim): fix lua_ls go to definitions across files

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -34,9 +34,10 @@ return {
           settings = {
             Lua = {
               workspace = {
-                library = vim.api.nvim_get_runtime_file("", true),
+                library = {
+                  vim.api.nvim_get_runtime_file("", true)
+                },
               },
-              telemetry = { enabled = false },
             },
           },
         },
