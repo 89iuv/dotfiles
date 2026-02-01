9cee8ee223ff7dd12f2d4ebb81951b4ea8185d50
Valentin Uveges <valentin.uveges@gmail.com>
2026-02-01 17:20:08 +0200

    feat(nvim): clean up

--- nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -9,17 +9,10 @@ return {
     "TmuxNavigatorProcessList",
   },
   keys = {
-    { "<c-h>", "<cmd>TmuxNavigateLeft<cr>", mode = { "n", "t" } },
-    { "<c-j>", "<cmd>TmuxNavigateDown<cr>", mode = { "n", "t" } },
-    { "<c-k>", "<cmd>TmuxNavigateUp<cr>", mode = { "n", "t" } },
-    { "<c-l>", "<cmd>TmuxNavigateRight<cr>", mode = { "n", "t" } },
-    { "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>", mode = { "n", "t" } },
+    { "<c-h>", "<cmd>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
   },
-  config = function()
-    vim.keymap.set("t", "<c-h>", "<cmd>TmuxNavigateLeft<cr>", { noremap = true })
-    vim.keymap.set("t", "<c-j>", "<cmd>TmuxNavigateDown<cr>", { noremap = true })
-    vim.keymap.set("t", "<c-k>", "<cmd>TmuxNavigateUp<cr>", { noremap = true })
-    vim.keymap.set("t", "<c-l>", "<cmd>TmuxNavigateRight<cr>", { noremap = true })
-    vim.keymap.set("t", "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>", { noremap = true })
-  end,
 }
