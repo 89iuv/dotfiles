7dfb14929e94933c837eeae9fa42d609d9869653
Valentin Uveges <valentin.uveges@gmail.com>
2026-02-06 16:26:37 +0200

    fix(ai): add back systemctl daemon-reload

--- install.sh
+++ install.sh
@@ -167,6 +167,7 @@ curl -fsSL https://ollama.com/download/ollama-linux-amd64.tar.zst \
 sudo useradd -r -s /bin/false -U -m -d /usr/share/ollama ollama
 sudo usermod -a -G ollama "$(whoami)"
 sudo cp ~/.dotfiles/ollama/ollama.service /etc/systemd/system/
+sudo systemctl daemon-reload
 sudo systemctl enable --now ollama.service
 # HACK: wait for ollama server to start
 # on docker build, ollama server needs to be started manually
