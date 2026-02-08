06a38eec45779566602a24725953f2fe5f506bc3
Valentin Uveges <valentin.uveges@gmail.com>
2026-02-08 12:06:25 +0200

    feat(git): use yello for border in lazygit, rework delta header

--- install.sh
+++ install.sh
@@ -77,16 +77,16 @@ bat cache --build
 # delta
 sudo dnf -y install git-delta
 git config --global core.pager delta
-git config --global include.path "$HOME/.config/delta/themes/catppuccin.gitconfig"
 git config --global interactive.diffFilter "delta --color-only"
+git config --global include.path "$HOME/.config/delta/themes/catppuccin.gitconfig"
 git config --global delta.features catppuccin-macchiato
 git config --global delta.true-colors "always"
-git config --global delta.navigate true
 git config --global delta.line-numbers true
 git config --global delta.commit-decoration-style "bold"
+git config --global delta.file-style "#b8c0e0"
 git config --global delta.file-decoration-style "ul #6e738d"
 git config --global delta.hunk-header-style omit
-git config --global delta.hunk-header-decoration-style "box"
+git config --global delta.hunk-header-decoration-style "ul #6e738d"
 git config --global alias.diff-unified "-c delta.hunk-header-style=auto -c delta.line-numbers=false diff"
 git config --global alias.diff-compare "-c delta.side-by-side=true diff"
 git config --global diff.colorMoved default
