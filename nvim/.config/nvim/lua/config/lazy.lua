973765de1a9bdce6c9b318bd076cd94a11a379c3
Valentin Uveges <valentin.uveges@gmail.com>
2026-04-22 12:32:29 +0300

    chore(lazy.nvim): update config with checker, UI, and performance settings

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -15,12 +15,40 @@ require('lazy').setup({
   spec = {
     { import = "plugins" },
   },
-  defaults = { lazy = true },
+  defaults = {
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
+  },
   install = { colorscheme = { "catppuccin" } },
-  checker = { enabled = false },
+  ui = {
+    backdrop = 60,
+    border = "single", -- this loads before vim.opt.winborder is set
+  },
+  checker = {
+    enabled = true, -- check for plugin updates periodically
+    notify = false, -- notify on update
+  }, -- automatically check for plugin updates
+  change_detection = {
+    notify = true,
+  },
   performance = {
     rtp = {
-      reset = false,
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        "matchparen",
+        -- "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
     },
   },
 })
