6ac2fdbd2d57833476555affdfcfbd1e7ff3823c
Valentin Uveges <valentin.uveges@gmail.com>
2026-07-22 08:18:51 +0300

    feat(terminal): update backup script

--- windows-terminal/scripts/backup.sh
+++ windows-terminal/scripts/backup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # Check if the WINDOWS_USER_NAME environment variable is set
+echo "Testing if the WINDOWS_USER_NAME environment variable is set..."
 if [ -z "$WINDOWS_USER_NAME" ]; then
 	echo "WINDOWS_USER_NAME is not set, use export WINDOWS_USER_NAME=<your_windows_user_name>"
 	exit 1
 fi
 
 # Backup Windows Terminal settings to the dotfiles directory
+echo "Backing up Windows Terminal settings..."
 cp /mnt/c/Users/"$WINDOWS_USER_NAME"/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json ~/.dotfiles/windows-terminal/config/
+
+echo "Backup completed successfully."
