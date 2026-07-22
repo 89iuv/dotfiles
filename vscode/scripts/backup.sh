b9c70ba853611df280925e8be02ef2b185d47c01
Valentin Uveges <valentin.uveges@gmail.com>
2026-07-22 08:18:19 +0300

    feat(vscode): update backup script and config

--- vscode/scripts/backup.sh
+++ vscode/scripts/backup.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 # Check if the WINDOWS_USER_NAME environment variable is set
+echo "Testing if the WINDOWS_USER_NAME environment variable is set..."
 if [ -z "$WINDOWS_USER_NAME" ]; then
 	echo "WINDOWS_USER_NAME is not set, use export WINDOWS_USER_NAME=<your_windows_user_name>"
 	exit 1
 fi
 
 # Backup VSCode settings and keybindings to the dotfiles directory
+echo "Backing up VSCode settings and keybindings..."
 cp /mnt/c/Users/"$WINDOWS_USER_NAME"/AppData/Roaming/Code/User/settings.json ~/.dotfiles/vscode/config/
 cp /mnt/c/Users/"$WINDOWS_USER_NAME"/AppData/Roaming/Code/User/keybindings.json ~/.dotfiles/vscode/config/
 
 # Backup VSCode extensions to a text file
+echo "Backing up VSCode extensions..."
 code --list-extensions > ~/.dotfiles/vscode/config/extensions.txt
+
+echo "Backup completed successfully."
